f34ac89280eb8be764106eee629c09bd253e75a8
Hector Luis Barrientos Margolles <hector_barrientos@stratesys-ts.com>
2017-05-10 16:48:42 +0200

    Turn a little bit more complex recursion snipets

--- recursion.js
+++ recursion.js
@@ -1,20 +1,47 @@
+
+let result = []
+const number = 15
+let countDownFrom = (num, result) => {
+  if (num >= 0){
+    result.push(num);
+    countDownFrom(num - 1, result)
+  }
+  return result
+}
+//console.log(countDownFrom(number, result))
+
 let categories = [
-    {id: 'animals', 'parent':null},
-    {id: 'mammals', 'parent': 'animals'},
-    {id: 'cats','parent': 'mammals' },
-    {id: 'dogs', 'parent': 'mammals'},
-    {id: 'buldog', 'parent':'dogs'},
-    {id: 'persian', 'parent':'cats'},
-    {id: 'chauchau', 'parent':'dogs'},
-    {id: 'siamese', 'parent':'cats'},
-    {id: 'pointer', 'parent':'dogs'},
+{id: 'animals', 'parent':null},
+{id: 'mammals', 'parent': 'animals'},
+{id: 'cats','parent': 'mammals' },
+{id: 'dogs', 'parent': 'mammals'},
+{id: 'buldog', 'parent':'dogs'},
+{id: 'persian', 'parent':'cats'},
+{id: 'chauchau', 'parent':'dogs'},
+{id: 'siamese', 'parent':'cats'},
+{id: 'pointer', 'parent':'dogs'},
 ]
 
-let countDownFrom = (num, result) => {
-    if (num >= 0){
-        result.push(num);
-        countDownFrom(num - 1, result)
-    } 
-    return result
+let makeTree = (categories, parent) => {
+  let node = {}
+  categories
+    .filter(cat => cat.parent === parent)
+    .forEach(cat =>
+        node[cat.id] = makeTree(categories, cat.id)
+        )
+    return node
 }
-console.log(categories)
+//console.log(JSON.stringify(makeTree(categories, null), null, 2))
+
+var ages = [32, 33, 16, 40];
+let oldies = (ages, criteria) => {
+  let res = []
+    ages
+    .filter(a => a > criteria)
+    .forEach(a => {
+      res.push(a+5)
+    })
+  return res
+}
+console.log(oldies(ages, 20))
+
